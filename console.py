21820f4802b2fe642e3428b35fd8f2f210a69336
Alex <alexgjohn@outlook.com>
2023-04-26 22:25:47 +0100

    completed mvp and worked on extensions

--- console.py
+++ console.py
@@ -4,11 +4,29 @@ from models.book import Book
 import repositories.author_repository as author_repo
 import repositories.book_repository as book_repo
 
-# author1 = Author("RF Kuang")
-# author_repo.save(author1)
+book_repo.delete_all()
+author_repo.delete_all()
 
-# book1 = Book("Babel", "Fiction", author1)
-# book2 = Book("The Poppy War", "Fiction", author1)
-# book_repo.save(book1)
-# book_repo.save(book2)
+
+author1 = Author("RF Kuang")
+author2 = Author("China Mieville")
+author3 = Author("Tabitha Snooz")
+best_author = Author("Alex John")
+author_repo.save(author1)
+author_repo.save(author2)
+author_repo.save(author3)
+author_repo.save(best_author)
+
+book1 = Book("Babel", "Fiction", author1)
+book2 = Book("The Poppy War", "Fiction", author1)
+book3 = Book("The City and The City", "Weird Fiction", author2)
+book4 = Book("The Benefits of Bedtime", "Self Help", author3)
+book5 = Book("Absolute Basics of Python", "Education", best_author)
+book6 = Book("Trust the Process? The Skeletons in the CodeClan Closet", "Investigative Journalism", best_author)
+book_repo.save(book1)
+book_repo.save(book2)
+book_repo.save(book3)
+book_repo.save(book4)
+book_repo.save(book5)
+book_repo.save(book6)
 
--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -9,12 +9,42 @@ def books():
     books = book_repo.select_all()
     return render_template('books/index.jinja', books_in_list = books)
 
-@books_blueprint.route('/books/new')
+@books_blueprint.route('/books/new', methods = ['GET'])
 def new_book():
     authors = author_repo.select_all()
     return render_template('books/new.jinja', authors_on_file = authors)
+
+
+@books_blueprint.route('/books', methods = ['POST'])
+def add_book():
+    title = request.form['title']
+    genre = request.form['genre']
+    author_id = request.form['author_id']
+    author = author_repo.select(author_id)
+    book = Book(title, genre, author)
+    book_repo.save(book)
+    return redirect('/books')
+
+
+# @tasks_blueprint.route("/tasks",  methods=['POST'])
+# def create_task():
+#     description = request.form['description']
+#     user_id     = request.form['user_id']
+#     duration    = request.form['duration']
+#     completed   = request.form['completed']
+#     user        = user_repository.select(user_id)
+#     task        = Task(description, user, duration, completed)
+#     task_repository.save(task)
+#     return redirect('/tasks')
 
 @books_blueprint.route('/books/<id>')
 def show_book(id):
     book = book_repo.select(id)
     return render_template('books/show.jinja', book = book)
+
+@books_blueprint.route('/books/<id>/delete', methods = ['POST'])
+def delete_book(id):
+    book_repo.delete(id)
+    return redirect('/books')
+
+
